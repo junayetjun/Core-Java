5d5daf5af936dbf9530de72a269d1f5b9e280ac6
Samim Junayet Istiaq <junayet03@gmail.com>
2025-04-10 01:02:34 +0600

    10-04-25

---
+++ SelfPractice/src/selfpractice/SelfPractice.java
@@ -0,0 +1,32 @@
+
+package selfpractice;
+
+import java.util.Scanner;
+
+
+public class SelfPractice {
+
+   
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter number ");
+        int number = input.nextInt();
+        
+        int count =1;
+        
+        for(int i =1; i<=number; i++){
+            
+            count++;
+        
+        }
+        System.out.println("No is " + number);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
