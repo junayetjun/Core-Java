91bec061d15923d571fa32e43f264f9307251e83
junayetjun <junayet03@gmail.com>
2025-04-26 19:06:12 +0600

    26-04-25

---
+++ ProblemSolve/src/problemsolve/ProblemSolve.java
@@ -0,0 +1,30 @@
+package problemsolve;
+
+import java.util.Scanner;
+
+public class ProblemSolve {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+//        System.out.println("Enter the number");
+//        int a = s.nextInt();
+//        System.out.println("Enter the number float");
+//        double d = s.nextDouble();
+//
+//        // System.out.println("Enter the letter");
+//        String c = s.nextLine();
+//
+//        System.out.println(c);
+//        System.out.println(d);
+//        System.out.println(a);
+
+        String name = "Alice";
+        int age = 9;
+
+        System.out.printf("Name: %s, Age: %05d\n", name, age);
+
+    }
+
+}
