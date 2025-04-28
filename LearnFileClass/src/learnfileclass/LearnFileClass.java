54a1d9f7543ad2a96c1b0a9d38b490d044c76e5d
junayetjun <junayet03@gmail.com>
2025-04-28 19:11:31 +0600

    28-04-25

--- LearnFileClass/src/learnfileclass/LearnFileClass.java
+++ LearnFileClass/src/learnfileclass/LearnFileClass.java
@@ -14,34 +14,40 @@ public class LearnFileClass {
 
         File f = new File("C:\\Users\\Admin\\Desktop\\sam.txt");
 
-        try { //This is for read file 
-            FileReader fr = new FileReader(f);
+//        try { //This is for read file 
+//            FileReader fr = new FileReader(f);
+//
+//            System.out.println(fr.read());
 
-            System.out.println(fr.read());
-
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(LearnFileClass.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        try { // Print same file repeated 
-            PrintWriter pw = new PrintWriter("C:\\Users\\Admin\\Desktop\\junayet.txt");
-            for (int i = 1; i <= 10; i++) {
-//                PrintWriter pw = new PrintWriter("C:\\Users\\Admin\\Desktop\\junayet.txt" + (i + 1));
-
-                pw.println("I Love Java " + (i + 1));
-
-            }
-            pw.close();
-//            PrintWriter pw =new Pri ntWriter("C:\\Users\\Admin\\Desktop\\junayet.txt");
-//            
-//            
-//            pw.println("I Love Java.");
-//            pw.println("I Love Coding.");
-//            pw.println("I Love Technology.");
-
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(LearnFileClass.class.getName()).log(Level.SEVERE, null, ex);
-        }
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(LearnFileClass.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//
+//        try { // Print same file repeated 
+//            PrintWriter pw = new PrintWriter("C:\\Users\\Admin\\Desktop\\junayet.txt");
+//            for (int i = 1; i <= 10; i++) {
+////                PrintWriter pw = new PrintWriter("C:\\Users\\Admin\\Desktop\\junayet.txt" + (i + 1));
+//
+//                pw.println("I Love Java " + (i + 1));
+//
+//            }
+//            pw.close();
+////            PrintWriter pw =new Pri ntWriter("C:\\Users\\Admin\\Desktop\\junayet.txt");
+////            
+////            
+////            pw.println("I Love Java.");
+////            pw.println("I Love Coding.");
+////            pw.println("I Love Technology.");
+//
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(LearnFileClass.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        
+        
+        
+        PrintWriter ps = new PrintWriter("C:\\Users\\Admin\\Desktop\\junayet.html");
+        
+        
 
     }
 
