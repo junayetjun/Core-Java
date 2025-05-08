6574c5c311d0966081377c3c4bafd970010e28ec
Samim Junayet Istiaq <junayet03@gmail.com>
2025-05-08 13:56:56 +0600

    09-05-25

---
+++ CollectionsListSort/src/collectionslistsort/CollectionsListSort.java
@@ -0,0 +1,20 @@
+
+package collectionslistsort;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class CollectionsListSort {
+
+    
+    public static void main(String[] args) {
+       
+        long[] myArray = {10,5,4,8,2,7,9,6,3,1};
+        
+       Arrays.sort(myArray);
+        
+        System.out.println(Arrays.toString(myArray));
+    }
+    
+}
