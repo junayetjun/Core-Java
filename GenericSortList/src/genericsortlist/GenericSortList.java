56fd42457ee63bfb5bcaed288e3b0b15daca88e7
junayetjun <junayet03@gmail.com>
2025-05-08 19:02:45 +0600

    08-05-25

---
+++ GenericSortList/src/genericsortlist/GenericSortList.java
@@ -0,0 +1,31 @@
+
+package genericsortlist;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class GenericSortList {
+
+    
+    public static void main(String[] args) {
+      
+        
+        List<Integer> number = new LinkedList<>();
+        
+        number.add(5);
+        number.add(4);
+        number.add(10);
+        number.add(2);
+        number.add(7);
+        
+        System.out.println(number);
+        Collections.sort(number);
+        
+        System.out.println(number);
+        
+    }
+    
+}
