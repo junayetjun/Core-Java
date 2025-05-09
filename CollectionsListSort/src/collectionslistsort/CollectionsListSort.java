99b5368631fcbd4d88eb09f14d0fddd42fe45010
Samim Junayet Istiaq <junayet03@gmail.com>
2025-05-09 16:21:12 +0600

    10-05-25

--- CollectionsListSort/src/collectionslistsort/CollectionsListSort.java
+++ CollectionsListSort/src/collectionslistsort/CollectionsListSort.java
@@ -3,6 +3,8 @@ package collectionslistsort;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class CollectionsListSort {
@@ -14,7 +16,25 @@ public class CollectionsListSort {
         
        Arrays.sort(myArray);
         
-        System.out.println(Arrays.toString(myArray));
+        //System.out.println(Arrays.toString(myArray));
+        
+        List<Integer> numbers = new LinkedList<>();
+        
+        numbers.add(4);
+        numbers.add(8);
+        numbers.add(5);
+        numbers.add(1);
+        numbers.add(10);
+        numbers.add(7);
+        numbers.add(12);
+        numbers.add(6);
+        numbers.add(2);
+        
+        Collections.sort(numbers);
+        
+        
+        System.out.println(numbers);
+        
     }
     
 }
