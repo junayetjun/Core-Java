dcf0696ddac710afbf26df88a562096a19ad490c
Samim Junayet Istiaq <junayet03@gmail.com>
2025-05-20 02:00:32 +0600

    add

--- ExceptionHandling/src/exceptionhandling/ExceptionHandling.java
+++ ExceptionHandling/src/exceptionhandling/ExceptionHandling.java
@@ -1,27 +1,38 @@
-
 package exceptionhandling;
 
+import java.util.Arrays;
 import java.util.Scanner;
-
 
 public class ExceptionHandling {
 
     public static void main(String[] args) {
-      
-        
+
+//        try {
+//            int i = 10/0;
+//        } catch (ArithmeticException e) {
+//            System.out.println(e);
+//        }
+//        
+//        try {
+//            int[] myArray = {1,2,3};
+//            
+//            int res = myArray[4];
+//           // System.out.println(Arrays.toString(myArray));
+//            System.out.println(res);
+//        } catch (IndexOutOfBoundsException e) {
+//            System.out.println(e);
+//        }
+//        
         try {
-            Scanner s = new Scanner(System.in);
-            System.out.println("Enter the number");
-            int input =s.nextInt();
-  
-            
-        } catch (Exception e) {
+            Object[] array = new String[5];
+           // array[0] = "Hello"; // OK
+            array[1] = 123;
+
+        } catch (ArrayStoreException e) {
             System.out.println(e);
         }
-        
-        
-        
-        
+
+        // Throws ArrayStoreException
     }
-    
+
 }
--- ScannerUseToRead/src/scannerusetoread/ScannerUseToRead.java
+++ ScannerUseToRead/src/scannerusetoread/ScannerUseToRead.java
@@ -11,25 +11,26 @@ public class ScannerUseToRead {
 
     public static void main(String[] args) {
 
-        File f = new File("C:\\Users\\Admin\\Desktop\\junayet.txt");
+        File f = new File("C:\\Users\\User\\Desktop\\junayet.txt");
         try {
             Scanner s = new Scanner(f);
 
-            while (s.hasNext()) {
-                
-                String st = s.nextLine();
-                
-                String[] myArray = st.split(" ");
-                
-                System.out.println(Arrays.toString(myArray));
-
-            }
-            s.close();
-//            System.out.println(s.nextLine());
-//             while(s.hasNext()){
-//                 System.out.println(s.nextLine());
-//             
-//             }
+//            while (s.hasNext()) {
+//                
+//                String st = s.nextLine();
+//                
+//                String[] myArray = st.split(" ");
+//                
+//                System.out.println(Arrays.toString(myArray));
+//
+//            }
+//            s.close();
+            System.out.println(s.nextLine());
+             while(s.hasNext()){
+                 System.out.println(s.nextLine());
+             
+             }
+             s.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(ScannerUseToRead.class.getName()).log(Level.SEVERE, null, ex);
         }
