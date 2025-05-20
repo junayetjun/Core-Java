f95dc00499421bdfc33bb6af6c18292f090047fd
Samim Junayet Istiaq <junayet03@gmail.com>
2025-05-20 10:20:54 +0600

    add

--- ExceptionHandling/src/exceptionhandling/ExceptionHandling.java
+++ ExceptionHandling/src/exceptionhandling/ExceptionHandling.java
@@ -13,26 +13,27 @@ public class ExceptionHandling {
 //            System.out.println(e);
 //        }
 //        
-//        try {
-//            int[] myArray = {1,2,3};
-//            
-//            int res = myArray[4];
-//           // System.out.println(Arrays.toString(myArray));
-//            System.out.println(res);
-//        } catch (IndexOutOfBoundsException e) {
-//            System.out.println(e);
-//        }
-//        
         try {
-            Object[] array = new String[5];
-           // array[0] = "Hello"; // OK
-            array[1] = 123;
-
-        } catch (ArrayStoreException e) {
+            int[] myArray = {1,2,3};
+            myArray[4]=3;
+            //int res = myArray[4];
+            System.out.println(Arrays.toString(myArray));
+            //System.out.println(res);
+        } catch (IndexOutOfBoundsException e) {
             System.out.println(e);
         }
+//        
+//        try {
+//             // Throws ArrayStoreException
+//            Object[] array = new String[5];
+//           // array[0] = "Hello"; // OK
+//            array[1] = 123;
+//
+//        } catch (ArrayStoreException e) {
+//            System.out.println(e);
+//        }
 
-        // Throws ArrayStoreException
+       
     }
 
 }
